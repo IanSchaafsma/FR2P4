a86e2aaf2131b6b3d309cbe163cae96a65d0db45
Ian <33319@ma-web.nl>
2023-05-12 14:03:43 +0200

    add les 8 agenda app

--- Agenda/js/main.js
+++ Agenda/js/main.js
@@ -27,25 +27,39 @@ class Agenda{
     renderer;
     header;
     month;
+    htmlElement;
 
     constructor(data){
+        this.htmlElement = document.createElement("article");
+        this.htmlElement.classList.add("agenda");
         this.data = data;
-        console.log(data);
         this.renderer = new Renderer();
-        this.header = new Header(data.name);
+        this.renderer.render("body", this.htmlElement);
+        this.header = new Header(this, data.name);
         this.month = new Month(this, data.days);
+    }
+
+    render(placeToRender, WhatToRender){
+        this.renderer.render(placeToRender, WhatToRender);
     }
 }
 
 class Renderer{
-
+    render(placeToRender, WhatToRender){
+        document.querySelector(placeToRender).appendChild(WhatToRender);
+    }
 }
 
 class Header{
     nameOfMonth;
-    constructor(nameOfMonth){
+    htmlElement;
+    agenda;
+
+    constructor(agenda, nameOfMonth){
+        this.agenda = agenda;
+        this.htmlElement = document.createElement("header");
+        this.agenda.render(".agenda", this.htmlElement);
         this.nameOfMonth = nameOfMonth;
-        console.log(nameOfMonth);
     }
 }
 
@@ -53,11 +67,13 @@ class Month{
     day = [];
     agenda;
     numberOfDays;
+    htmlElement;
 
     constructor(agenda, numberOfDays){
-        this.numberOfDays = numberOfDays;
-        console.log(this.numberOfDays);
         this.agenda = agenda;
+        this.htmlElement = document.createElement("ul");
+        this.agenda.render(".agenda", this.htmlElement);
+        this.numberOfDays = numberOfDays;
         for(let i = 0; i < numberOfDays; i++){
             this.day.push(new Day(this));
         }
@@ -66,12 +82,13 @@ class Month{
 
 class Day{
     month;
+    htmlElement;
     
     constructor(month){
+        this.htmlElement = document.createElement("li");
         this.month = month;
     }
     
 }
 
 const ianZijnAgenda = new AgendaApp();
-console.log(ianZijnAgenda);
